14d4ba038ad031bbaa4cb744ee7f6427467b5498
Kgodiso Loui Mashile <135594219+KgodisoMashile@users.noreply.github.com>
2023-09-07 15:26:41 +0200

    Update RegistrationApp.java

--- src/registrationapp/RegistrationApp.java
+++ src/registrationapp/RegistrationApp.java
@@ -1,6 +1,6 @@
 package registrationapp;
 import za.ac.tut.registerframe.RegisterFrame;
-
+//this file belongs to Kgodiso
 public class RegistrationApp {
 
     public static void main(String[] args) 
