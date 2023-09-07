ae0bf25f38999f7226ecaaac759394e8585e5608
Kgodiso <Kgodiso@DESKTOP-F26CU0F.mshome.net>
2023-09-07 15:21:18 +0200

    This is the first commit for this simple registration application.

---
+++ src/registrationapp/RegistrationApp.java
@@ -0,0 +1,12 @@
+package registrationapp;
+import za.ac.tut.registerframe.RegisterFrame;
+
+public class RegistrationApp {
+
+    public static void main(String[] args) 
+    {
+            //class instantiation
+            RegisterFrame rf=new RegisterFrame();
+    }
+    
+}
